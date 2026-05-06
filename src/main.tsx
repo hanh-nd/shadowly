88fde6ea76a0d7bb17f5890ef25374dd8bad96fe
hanhnd <hanhnd156@gmail.com>
2026-05-07 06:50:46 +0700

    feat: add mobile debugger

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
+// Initialize Eruda for mobile debugging
+import('eruda').then((eruda) => eruda.default.init());
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
